6de4f1a1b7fd7a8664bb27fcf22fb1f47bc6ec02
Mandar Kale <mandarakale@gmail.com>
2025-07-19 20:26:11 +0530

    Initialize Spring Boot Hello World app: add main class, controller, and project structure. Fix main class error.

---
+++ src/main/java/com/bake4kids/helloworld/HelloWorldApplication.java
@@ -0,0 +1,11 @@
+package com.bake4kids.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+} 
